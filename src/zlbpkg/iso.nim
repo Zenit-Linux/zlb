79a2e6e6dfb0a1eaf527c815f7cb4786dbbafe85
michal92299 <chudybyczek2137@gmail.com>
2026-08-26 09:18:54 +0200

    Update iso.nim

--- src/zlbpkg/iso.nim
+++ src/zlbpkg/iso.nim
@@ -26,9 +26,9 @@ proc squashRootfs(rootfs, outSfs: string) =
 proc layoutIsoTree(p: ProjectPaths, arch: string, sfsPath, brandingDir: string): string =
   let isoTree = p.workDir(arch) / "isotree"
   removeDir(isoTree)
-  createDir(isoTree / "zenith" / arch)
+  createDir(isoTree / "zenit" / arch)
   createDir(isoTree / "boot" / "grub")
-  copyFileWithPermissions(sfsPath, isoTree / "zenith" / arch / "airootfs.sfs")
+  copyFileWithPermissions(sfsPath, isoTree / "zenit" / arch / "airootfs.sfs")
   if dirExists(brandingDir):
     createDir(isoTree / "branding")
     for kind, path in walkDir(brandingDir):
@@ -38,9 +38,9 @@ proc layoutIsoTree(p: ProjectPaths, arch: string, sfsPath, brandingDir: string):
 
 proc writeGrubCfg(isoTree, distroName, arch: string) =
   let cfgPath = isoTree / "boot" / "grub" / "grub.cfg"
-  ## `installer=1` na linii poleceń jądra jest tym, co (w zenith-main's
-  ## overlays/system/usr/local/bin/zenith-session-select) odróżnia sesję
-  ## live-only od pełnoekranowego Zenith Installer -- podobnie jak
+  ## `installer=1` na linii poleceń jądra jest tym, co (w zenit-main's
+  ## overlays/system/usr/local/bin/zenit-session-select) odróżnia sesję
+  ## live-only od pełnoekranowego Zenit Installer -- podobnie jak
   ## najnowsze Fedory oferują "Start Fedora" vs "Install Fedora" wprost
   ## z GRUB-a, zamiast dopiero z poziomu pulpitu live.
   let cfg = &"""
@@ -48,17 +48,17 @@ set timeout=5
 set default=0
 
 menuentry "Try/Live {distroName} ({arch})" {{
-  linux /boot/vmlinuz-linux boot=zenith arch={arch} quiet
+  linux /boot/vmlinuz-linux boot=zenit arch={arch} quiet
   initrd /boot/initramfs-linux.img
 }}
 
 menuentry "Install {distroName} ({arch})" {{
-  linux /boot/vmlinuz-linux boot=zenith arch={arch} installer=1 quiet
+  linux /boot/vmlinuz-linux boot=zenit arch={arch} installer=1 quiet
   initrd /boot/initramfs-linux.img
 }}
 
 menuentry "{distroName} ({arch}) - Safe graphics" {{
-  linux /boot/vmlinuz-linux boot=zenith arch={arch} nomodeset
+  linux /boot/vmlinuz-linux boot=zenit arch={arch} nomodeset
   initrd /boot/initramfs-linux.img
 }}
 """
